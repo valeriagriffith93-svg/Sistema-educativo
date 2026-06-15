0dd93b653bb24c77864f381ae4d8d2060456b052
Valeria Griffith <valeriagriffith93@gamil.com>
2026-06-14 19:28:23 -0500

    Fix Render port

--- app.py
+++ app.py
@@ -1295,8 +1295,6 @@ def mis_notas():
         notas=notas
     )
 
-if __name__ == '__main__':
-    app.run(debug=True) 
-
-if __name__ == '__main__':
-    app.run(debug=True) 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
